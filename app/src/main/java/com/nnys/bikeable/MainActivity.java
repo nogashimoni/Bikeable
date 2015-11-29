d20c0736c41b2f3cc4b4577468d594ae8420029d
Sharon <sharonaftaly@gmail.com>
2015-11-29 16:56:05 +0200

    Added pathes from Iria: - map now shows paths from Iria when clicking, and removes when clicking again - changed the way clear map works to not clear the bike paths

--- app/src/main/java/com/nnys/bikeable/MainActivity.java
+++ app/src/main/java/com/nnys/bikeable/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         ImageButton button = (ImageButton) findViewById(R.id.startButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
