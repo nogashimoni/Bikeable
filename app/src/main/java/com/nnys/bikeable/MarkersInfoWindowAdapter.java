a6b4e9989e7debc14252268cb669868e0fcd7f00
Sharon <sharonaftaly@gmail.com>
2016-01-05 20:40:47 +0200

    Fixed problem where the red markers were showing bike data

--- app/src/main/java/com/nnys/bikeable/MarkersInfoWindowAdapter.java
+++ app/src/main/java/com/nnys/bikeable/MarkersInfoWindowAdapter.java
@@ -7,16 +7,25 @@ import android.view.View;
 import android.widget.TextView;
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.model.Marker;
-/**
- * Created by Yishay on 1/4/2016.
- */
+
+
 public class MarkersInfoWindowAdapter implements InfoWindowAdapter {
 
     private View popup = null;
     private LayoutInflater inflater = null;
+    TextView tvTitle;
+    TextView tvBike;
+    TextView tvStands;
 
     MarkersInfoWindowAdapter(LayoutInflater inflater) {
+
         this.inflater = inflater;
+
+        popup = inflater.inflate(R.layout.info_window_layout, null);
+
+        tvTitle= (TextView) popup.findViewById(R.id.title);;
+        tvBike =  (TextView) popup.findViewById(R.id.bike_data);
+        tvStands =  (TextView) popup.findViewById(R.id.stands_data);
     }
 
     @Override
@@ -27,35 +36,25 @@ public class MarkersInfoWindowAdapter implements InfoWindowAdapter {
     @SuppressLint("InflateParams")
     @Override
     public View getInfoContents(Marker marker) {
-        //if (popup == null) {
-            //popup = inflater.inflate(R.layout.info_window_layout, null);
-        //}
 
         if (marker.getTitle().equals("TelOFun")) {
-            if (popup == null) {
-                popup = inflater.inflate(R.layout.info_window_layout, null);
-            }
-            TextView tvBike;
+
             int stationId = Integer.parseInt(marker.getSnippet());
             TelOFunStation station = IriaData.getTelOfanStationsDict().get(stationId);
             int bikesAvailable = station.getNumOfBikesAvailable();
             int standsAvailable = station.getNumOfStandsAvailable();
             String stationName = station.getStationName();
 
-            tvBike = (TextView) popup.findViewById(R.id.title);
-            tvBike.setText(stationName);
-            tvBike = (TextView) popup.findViewById(R.id.bike_data);
+            tvTitle.setText(stationName);
+            tvBike.setVisibility(View.VISIBLE);
             tvBike.setText("bikes available: " + bikesAvailable);
-            tvBike = (TextView) popup.findViewById(R.id.stands_data);
-            tvBike.setText("stands available: " + standsAvailable);
+            tvStands.setVisibility(View.VISIBLE);
+            tvStands.setText("stands available: " + standsAvailable);
         }
         else {
-            if (popup == null) {
-                popup = inflater.inflate(R.layout.info_window_place, null);
-            }
-            TextView tvPlace;
-            tvPlace = (TextView) popup.findViewById(R.id.title);
-            tvPlace.setText(marker.getTitle());
+            tvTitle.setText(marker.getTitle());
+            tvBike.setVisibility(View.GONE);
+            tvStands.setVisibility(View.GONE);
         }
 
         return (popup);
